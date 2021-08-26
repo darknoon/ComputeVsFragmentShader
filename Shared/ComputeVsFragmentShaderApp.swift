8f4736776125d9486a9c4ad9f929e48000d0d2a5
Andrew Pouliot <andrew@darknoon.com>
2021-08-26 15:45:46 -0400

    Initial Commit

---
+++ Shared/ComputeVsFragmentShaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputeVsFragmentShaderApp.swift
+//  Shared
+//
+//  Created by Andrew Pouliot on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ComputeVsFragmentShaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
